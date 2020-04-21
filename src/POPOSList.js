5814b4a323cef09d5e6be763ff3bf53eccd30edf
Rediet8abere <redietaberegesesse@gmail.com>
2020-04-21 13:54:14 -0700

    hours

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -3,43 +3,28 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
-import './POPOSSpace.css';
+import data from './sfpopos-data.json'
 
 function POPOSList() {
+
+  const spaces = data.map(( { title, address, images, hours } ) => {
+
   return (
-   <div className="POPOSList">
-     <POPOSSpace
-       name="50 California Street"
-       address="50 California St."
-       image="50-california-st.jpg"
-     />
-     <POPOSSpace
-     name="100 Pine Street"
-     address="100 Pine Street St."
-     image="100-Pine.jpg"
-     />
-     <POPOSSpace
-     name="101-california"
-     address="101-california St"
-     image="101-california.jpg"
-     />
-     <POPOSSpace
-     name="roof garden"
-     address="343 sansome roof garden"
-     image="343-sansome-roof-garden.jpg"
-     />
-     <POPOSSpace
-     name="street plaza"
-     address="525 market street plaza"
-     image="525-market-street-plaza.jpg"
-     />
-     <POPOSSpace
-     name="555-california"
-     address="555-california"
-     image="555-california.jpg"
-     />
-   </div>
- )
+    <POPOSSpace
+      key={title}
+      name={title}
+      address={address}
+      image={images[0]}
+      hours={hours}
+    />
+  )
+})
+
+  return (
+    <div className="POPOSList">
+      { spaces }
+    </div>
+  )
 }
 
 export default POPOSList
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -4,11 +4,12 @@ import React from 'react'
 import './POPOSSpace.css';
 
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, address, hours } = props
   return (
     <div>
       <img src={`${process.env.PUBLIC_URL}images/${image}`} alt="california"/>
       <h1>{name}</h1>
+      <h2>{hours}</h2>
       <div>{address}</div>
     </div>
   )
